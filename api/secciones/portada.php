df575202838fd78d71505ad5bcf715a3b9fac838
Ruben Cruz <acostakaku420@gmail.com>
2025-02-18 12:37:04 -0500

    Update portada.php

--- api/secciones/portada.php
+++ api/secciones/portada.php
@@ -2,10 +2,10 @@
 /*****
 SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
-
 $portada = [
-"titulo" => "Diario El Hocicón, "Pobre, pero Honrado"",
-"autor" => "XXXX",
-"resumen" => "El resumen Febrero 2025",
+"titulo" => "Tendencias",
+"autor" => "sistemas@uns",
+"resumen" => "Las ultimas tendencias 2025",
 ];
 ?>
+
