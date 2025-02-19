4afc747ab89cf6a26838e8c4c9d58d42d20df31f
Ruben Cruz <acostakaku420@gmail.com>
2025-02-19 07:01:32 -0500

    Update contenedores.php

--- api/secciones/contenedores.php
+++ api/secciones/contenedores.php
@@ -3,9 +3,21 @@
 SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
-$contenedores = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+$contenedores1 = [
+    "titulo" => "Docker lanza una nueva versión con mejoras de seguridad",
+    "autor" => "Docker Inc.",
+    "resumen" => "La última actualización de Docker incorpora mejoras en la gestión de vulnerabilidades y una optimización en la ejecución de contenedores."
+];
+
+$contenedores2 = [
+    "titulo" => "Kubernetes alcanza un nuevo hito en adopción empresarial",
+    "autor" => "Cloud Native Computing Foundation (CNCF)",
+    "resumen" => "Un estudio revela que más del 80% de las empresas han adoptado Kubernetes para la gestión de contenedores en producción."
+];
+
+$contenedores3 = [
+    "titulo" => "Podman se posiciona como alternativa ligera a Docker",
+    "autor" => "Red Hat",
+    "resumen" => "Podman gana popularidad como una alternativa sin daemon a Docker, con ventajas en seguridad y compatibilidad con Kubernetes."
 ];
 ?>
