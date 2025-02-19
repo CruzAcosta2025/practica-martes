0315b987aa1ed7e950f4ed3599860522d643d2e7
Ruben Cruz <acostakaku420@gmail.com>
2025-02-19 07:02:58 -0500

    Update cultura.php

--- api/secciones/cultura.php
+++ api/secciones/cultura.php
@@ -3,9 +3,21 @@
 SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
-$cultura = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+$cultura1 = [
+    "titulo" => "Descubren un mural maya de más de 1,000 años de antigüedad",
+    "autor" => "Instituto Nacional de Antropología e Historia (INAH)",
+    "resumen" => "Arqueólogos han hallado en México un mural maya bien conservado que podría revelar detalles sobre la vida cotidiana en la antigüedad."
+];
+
+$cultura2 = [
+    "titulo" => "Premios Óscar 2025: Lista de nominados y sorpresas",
+    "autor" => "Academia de Artes y Ciencias Cinematográficas",
+    "resumen" => "La Academia ha anunciado las nominaciones a los Premios Óscar 2025, con grandes sorpresas y una fuerte presencia del cine independiente."
+];
+
+$cultura3 = [
+    "titulo" => "Récord de asistencia en el Festival de Teatro de Edimburgo",
+    "autor" => "Organización del Festival de Edimburgo",
+    "resumen" => "El famoso festival de teatro ha registrado una asistencia histórica este año, consolidándose como uno de los eventos culturales más importantes del mundo."
 ];
 ?>
