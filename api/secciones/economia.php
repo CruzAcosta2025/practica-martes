d31d5884bec074d68606dddd2cb1e1e20391e952
Ruben Cruz <acostakaku420@gmail.com>
2025-02-19 07:04:49 -0500

    Update economia.php

--- api/secciones/economia.php
+++ api/secciones/economia.php
@@ -3,9 +3,21 @@
 SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
-$economia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+$economia1 = [
+    "titulo" => "Inflación global muestra signos de desaceleración",
+    "autor" => "Fondo Monetario Internacional (FMI)",
+    "resumen" => "Un informe del FMI revela que la inflación en varias economías ha comenzado a disminuir, gracias a medidas de política monetaria más estrictas."
+];
+
+$economia2 = [
+    "titulo" => "Bitcoin alcanza un nuevo máximo histórico",
+    "autor" => "CoinMarketCap",
+    "resumen" => "El precio del Bitcoin ha superado su récord anterior, impulsado por una creciente adopción institucional y la expectativa de nuevos ETFs de criptomonedas."
+];
+
+$economia3 = [
+    "titulo" => "Crecimiento del comercio electrónico sigue en alza",
+    "autor" => "Banco Mundial",
+    "resumen" => "El comercio electrónico ha experimentado un crecimiento exponencial en los últimos años, impulsado por la digitalización y cambios en los hábitos de consumo."
 ];
 ?>
