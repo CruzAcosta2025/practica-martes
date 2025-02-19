6fbb847402c5d81a546d0447dd74279fb544ddc1
Ruben Cruz <acostakaku420@gmail.com>
2025-02-19 09:08:03 -0500

    Update gente.php

--- api/secciones/gente.php
+++ api/secciones/gente.php
@@ -3,21 +3,9 @@
 SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
-$gente1 = [
+$gente = [
     "titulo" => "Joven de 16 años gana competencia mundial de ajedrez",
     "autor" => "Federación Internacional de Ajedrez (FIDE)",
     "resumen" => "Un adolescente ha sorprendido al mundo al vencer a grandes maestros en el campeonato mundial, convirtiéndose en el campeón más joven de la historia."
-];
-
-$gente2 = [
-    "titulo" => "Voluntarios ayudan a comunidades afectadas por desastres naturales",
-    "autor" => "Cruz Roja Internacional",
-    "resumen" => "Miles de voluntarios se han movilizado para brindar apoyo y recursos a familias afectadas por recientes fenómenos climáticos extremos."
-];
-
-$gente3 = [
-    "titulo" => "Famoso actor dona su salario para obras benéficas",
-    "autor" => "Revista People",
-    "resumen" => "Reconocido por su filantropía, un actor de Hollywood ha decidido donar el 100% de sus ganancias de su última película a organizaciones de caridad."
 ];
 ?>
