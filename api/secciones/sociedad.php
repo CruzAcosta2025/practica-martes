de1fb71fa5158f7074d644184a65210247c1bb91
Ruben Cruz <acostakaku420@gmail.com>
2025-02-19 08:59:28 -0500

    Update sociedad.php

--- api/secciones/sociedad.php
+++ api/secciones/sociedad.php
@@ -3,9 +3,21 @@
 SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
-$sociedad = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+$sociedad1 = [
+    "titulo" => "Aumento del voluntariado en comunidades vulnerables",
+    "autor" => "Red Solidaria",
+    "resumen" => "Cada vez más jóvenes se suman a programas de voluntariado para apoyar a comunidades en situación de riesgo."
+];
+
+$sociedad2 = [
+    "titulo" => "Nuevas políticas para la igualdad de género en el trabajo",
+    "autor" => "ONU Mujeres",
+    "resumen" => "Gobiernos y empresas implementan estrategias para reducir la brecha salarial y fomentar la inclusión laboral."
+];
+
+$sociedad3 = [
+    "titulo" => "El impacto de la tecnología en la vida cotidiana",
+    "autor" => "Forbes",
+    "resumen" => "Desde la inteligencia artificial hasta las redes sociales, la tecnología sigue transformando la forma en que vivimos y nos relacionamos."
 ];
 ?>
