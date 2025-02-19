02e25dac8c564ae68e723c533b7f68e717741654
Ruben Cruz <acostakaku420@gmail.com>
2025-02-19 07:11:56 -0500

    Update internacional.php

--- api/secciones/internacional.php
+++ api/secciones/internacional.php
@@ -3,9 +3,21 @@
 SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
-$internacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+$internacional1 = [
+    "titulo" => "Histórico acuerdo de paz firmado entre dos naciones",
+    "autor" => "ONU",
+    "resumen" => "Líderes de dos países en conflicto han firmado un tratado de paz, poniendo fin a décadas de tensiones y promoviendo la estabilidad en la región."
+];
+
+$internacional2 = [
+    "titulo" => "Cumbre del G20 debate sobre el cambio climático y la economía global",
+    "autor" => "BBC News",
+    "resumen" => "Los líderes de las principales economías del mundo se han reunido para discutir estrategias contra el cambio climático y medidas para el crecimiento económico."
+];
+
+$internacional3 = [
+    "titulo" => "Descubrimiento arqueológico en Egipto sorprende al mundo",
+    "autor" => "National Geographic",
+    "resumen" => "Arqueólogos han encontrado una tumba intacta de más de 4,000 años de antigüedad, con inscripciones que revelan nuevos detalles sobre la historia egipcia."
 ];
 ?>
