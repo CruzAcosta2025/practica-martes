61b2b16bd921293b4038df72ca97b069f6ccb4fc
Ruben Cruz <acostakaku420@gmail.com>
2025-02-19 09:06:34 -0500

    Update ciencia.php

--- api/secciones/ciencia.php
+++ api/secciones/ciencia.php
@@ -3,21 +3,9 @@
 SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
-$ciencia1 = [
+$ciencia = [
     "titulo" => "Nuevo avance en la lucha contra el Alzheimer",
     "autor" => "Universidad de Harvard",
     "resumen" => "Científicos han desarrollado una nueva terapia experimental que ralentiza significativamente el avance del Alzheimer en etapas tempranas."
-];
-
-$ciencia2 = [
-    "titulo" => "NASA logra producir oxígeno en Marte",
-    "autor" => "NASA",
-    "resumen" => "El experimento MOXIE a bordo del rover Perseverance ha conseguido generar oxígeno a partir del dióxido de carbono en la atmósfera marciana."
-];
-
-$ciencia3 = [
-    "titulo" => "Crean el primer material superconductor a temperatura ambiente",
-    "autor" => "Instituto de Tecnología de Massachusetts (MIT)",
-    "resumen" => "Investigadores han desarrollado un material que puede conducir electricidad sin resistencia a temperatura ambiente, revolucionando la industria energética."
 ];
 ?>
