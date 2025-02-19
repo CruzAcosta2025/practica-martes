162d78cfd574a7f9a5aac26f7958655af49ed131
Ruben Cruz <acostakaku420@gmail.com>
2025-02-19 09:09:21 -0500

    Update portada.php

--- api/secciones/portada.php
+++ api/secciones/portada.php
@@ -3,21 +3,9 @@
 SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
-$portada1 = [
+$portada = [
     "titulo" => "Innovaciones tecnológicas que marcarán el 2025",
     "autor" => "Juan Martinez",
     "resumen" => "Desde inteligencia artificial hasta avances en biotecnología, estas son las innovaciones que transformarán el mundo este año."
-];
-
-$portada2 = [
-    "titulo" => "Crisis climática: ¿Estamos a tiempo de revertir el daño?",
-    "autor" => "Albus Zacarias",
-    "resumen" => "Expertos advierten sobre el impacto del cambio climático y las medidas urgentes que deben tomarse a nivel global."
-];
-
-$portada3 = [
-    "titulo" => "La economía global frente a nuevos desafíos en 2025",
-    "autor" => "Jefferson Salvatierra",
-    "resumen" => "Inflación, mercados emergentes y nuevas políticas económicas definirán el panorama financiero de este año."
 ];
 ?>
