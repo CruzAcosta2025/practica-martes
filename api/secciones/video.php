4ef7ec2428b35bbdb8e0aa0a89829837d4170134
Ruben Cruz <acostakaku420@gmail.com>
2025-02-19 09:02:03 -0500

    Update video.php

--- api/secciones/video.php
+++ api/secciones/video.php
@@ -3,9 +3,21 @@
 SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
-$video = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+$video1 = [
+    "titulo" => "YouTube lanza nuevas herramientas de edición para creadores",
+    "autor" => "YouTube Blog",
+    "resumen" => "La plataforma incorpora funciones avanzadas de edición, permitiendo a los creadores mejorar la calidad de sus videos sin necesidad de software externo."
+];
+
+$video2 = [
+    "titulo" => "Twitch implementa nuevas políticas para mejorar la comunidad",
+    "autor" => "Twitch News",
+    "resumen" => "La plataforma de streaming refuerza sus normas de convivencia con el objetivo de reducir el acoso y mejorar la experiencia de los usuarios."
+];
+
+$video3 = [
+    "titulo" => "Netflix prueba opción de video interactivo en sus series",
+    "autor" => "Variety",
+    "resumen" => "El gigante del streaming explora nuevas experiencias interactivas que permitirán a los espectadores influir en el desarrollo de la historia."
 ];
 ?>
