ab32386e78661af3d90f6de092485c3fc46efbc6
Ruben Cruz <acostakaku420@gmail.com>
2025-02-19 07:03:55 -0500

    Update deportes.php

--- api/secciones/deportes.php
+++ api/secciones/deportes.php
@@ -3,9 +3,21 @@
 SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
-$deportes = [
-"titulo" => "UNIVERSITARIO BICAMPEON 2024",
-"autor" => "sistemas@uns",
-"resumen" => "En su centenario, Universitario de Deportes se corono campeon nacional",
+$deportes1 = [
+    "titulo" => "Lionel Messi gana su octavo Balón de Oro",
+    "autor" => "FIFA",
+    "resumen" => "El astro argentino ha sido galardonado nuevamente con el Balón de Oro, consolidándose como uno de los mejores futbolistas de la historia."
+];
+
+$deportes2 = [
+    "titulo" => "Juegos Olímpicos 2024: París se prepara para el evento",
+    "autor" => "Comité Olímpico Internacional",
+    "resumen" => "Las infraestructuras y logística están casi listas para recibir a miles de atletas y fanáticos en la capital francesa este verano."
+];
+
+$deportes3 = [
+    "titulo" => "Max Verstappen domina la temporada de Fórmula 1",
+    "autor" => "Red Bull Racing",
+    "resumen" => "El piloto neerlandés ha asegurado múltiples victorias en el campeonato, consolidándose como el favorito para llevarse el título mundial."
 ];
 ?>
