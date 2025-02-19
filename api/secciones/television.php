e0a3ae29c0e14cf1eeadafe67e0f3e7e05cd5159
Ruben Cruz <acostakaku420@gmail.com>
2025-02-19 09:09:54 -0500

    Update television.php

--- api/secciones/television.php
+++ api/secciones/television.php
@@ -3,21 +3,9 @@
 SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
-$television1 = [
+$television = [
     "titulo" => "Nueva serie de ciencia ficción rompe récords de audiencia",
     "autor" => "Netflix",
     "resumen" => "La última producción de Netflix ha sorprendido al público con una trama innovadora y efectos visuales impresionantes."
-];
-
-$television2 = [
-    "titulo" => "Canales de televisión abierta apuestan por el streaming",
-    "autor" => "Forbes",
-    "resumen" => "Grandes cadenas de televisión lanzan sus propias plataformas de streaming para competir con Netflix y Disney+."
-];
-
-$television3 = [
-    "titulo" => "Premios Emmy 2025: Lista de ganadores y sorpresas",
-    "autor" => "Hollywood Reporter",
-    "resumen" => "La última edición de los Emmy destacó las mejores producciones del año, con sorpresas en varias categorías."
 ];
 ?>
