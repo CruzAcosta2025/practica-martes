eb2c3d10afc191b85e4e8c4c77545a328c15e9cc
Ruben Cruz <acostakaku420@gmail.com>
2025-02-19 07:14:37 -0500

    Update openshift.php

--- api/secciones/openshift.php
+++ api/secciones/openshift.php
@@ -3,9 +3,21 @@
 SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
-$openshift = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+$openshift1 = [
+    "titulo" => "Red Hat OpenShift lanza nueva versión con mejoras en seguridad",
+    "autor" => "Red Hat",
+    "resumen" => "La nueva actualización de OpenShift incorpora parches de seguridad avanzados y mejor compatibilidad con Kubernetes para optimizar el despliegue de aplicaciones."
+];
+
+$openshift2 = [
+    "titulo" => "Empresas aumentan la adopción de OpenShift en la nube híbrida",
+    "autor" => "Forrester Research",
+    "resumen" => "Un estudio revela que OpenShift se ha convertido en la plataforma preferida para la implementación de soluciones en entornos híbridos y multi-nube."
+];
+
+$openshift3 = [
+    "titulo" => "OpenShift incorpora inteligencia artificial para optimizar despliegues",
+    "autor" => "TechCrunch",
+    "resumen" => "La integración de IA en OpenShift permite una gestión más eficiente de recursos, automatizando el escalado de aplicaciones en tiempo real."
 ];
 ?>
